952171c79759918ffb202ea84884154fa72f286b
bboure <benoit.boure@serverlessguru.com>
2022-09-02 14:50:54 +0200

    simplify

--- examples/setupJest.ts
+++ examples/setupJest.ts
@@ -1,11 +1,3 @@
 import { matchers } from 'sls-jest';
 
-if (expect !== undefined) {
-  expect.extend(matchers);
-} else {
-  console.error(
-    "Unable to find Jest's global expect." +
-      '\nPlease check you have added jest-extended correctly to your jest configuration.' +
-      '\nSee https://github.com/jest-community/jest-extended#setup for help.',
-  );
-}
+expect.extend(matchers);
