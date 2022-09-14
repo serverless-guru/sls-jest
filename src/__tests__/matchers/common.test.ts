297ddf06195ffeeb3d4479527d7adcc59acdac54
bboure <benoit.boure@serverlessguru.com>
2022-09-14 17:58:55 +0200

    test: Add unit tests

---
+++ src/__tests__/matchers/common.test.ts
@@ -0,0 +1,316 @@
+import { dynamodbItem } from '../../../lib';
+import {
+  toExist,
+  toExistAndMatchInlineSnapshot,
+  toExistAndMatchObject,
+  toExistAndMatchSnapshot,
+} from '../../../lib/matchers';
+import * as dynamodb from '../../../lib/matchers/dynamodb.internal';
+import * as s3 from '../../../lib/matchers/s3.internal';
+import { appSyncMappingTemplate, s3Object } from '../../helpers';
+
+describe('toExist', () => {
+  it('should accept dynamodbItem inputs', () => {
+    const spy = jest.spyOn(dynamodb, 'toExist').mockResolvedValue({
+      pass: true,
+      message: () => 'message',
+    });
+
+    expect(
+      toExist(
+        dynamodbItem({
+          tableName: 'table',
+          key: { id: 'id' },
+        }),
+      ),
+    ).resolves.toBeTruthy();
+    expect(spy.mock.calls[0][0]).toMatchInlineSnapshot(`
+      Object {
+        "_helperName": "dynamodbItem",
+        "key": Object {
+          "id": "id",
+        },
+        "tableName": "table",
+      }
+    `);
+
+    spy.mockRestore();
+  });
+
+  it('should accept s3 inputs', () => {
+    const spy = jest.spyOn(s3, 'toExist').mockResolvedValue({
+      pass: true,
+      message: () => 'message',
+    });
+
+    expect(
+      toExist(
+        s3Object({
+          bucketName: 'muy-bucket',
+          key: 'path/to/file.txt',
+        }),
+      ),
+    ).resolves.toBeTruthy();
+    expect(spy.mock.calls[0][0]).toMatchInlineSnapshot(`
+      Object {
+        "_helperName": "s3Object",
+        "bucketName": "muy-bucket",
+        "key": "path/to/file.txt",
+      }
+    `);
+
+    spy.mockRestore();
+  });
+
+  it('should not accept invalid inputs', () => {
+    expect(
+      toExist(
+        appSyncMappingTemplate({
+          template: 'template',
+          context: {},
+        }),
+      ),
+    ).rejects.toThrowErrorMatchingInlineSnapshot(
+      `"appSyncMappingTemplate() is not compatible with the toExist() matcher"`,
+    );
+  });
+
+  it('should not accept unknown inputs', () => {
+    expect(toExist({})).rejects.toThrowErrorMatchingInlineSnapshot(
+      `"Invalid matcher helper input. Please use one of the provided helpers."`,
+    );
+  });
+});
+
+describe('toExistAndMatchObject', () => {
+  it('should accept dynamodbItem inputs', () => {
+    const spy = jest
+      .spyOn(dynamodb, 'toExistAndMatchObject')
+      .mockResolvedValue({
+        pass: true,
+        message: () => 'message',
+      });
+
+    expect(
+      toExistAndMatchObject(
+        dynamodbItem({
+          tableName: 'table',
+          key: { id: 'id' },
+        }),
+      ),
+    ).resolves.toBeTruthy();
+    expect(spy.mock.calls[0][0]).toMatchInlineSnapshot(`
+      Object {
+        "_helperName": "dynamodbItem",
+        "key": Object {
+          "id": "id",
+        },
+        "tableName": "table",
+      }
+    `);
+
+    spy.mockRestore();
+  });
+
+  it('should accept s3 inputs', () => {
+    const spy = jest.spyOn(s3, 'toExistAndMatchObject').mockResolvedValue({
+      pass: true,
+      message: () => 'message',
+    });
+
+    expect(
+      toExistAndMatchObject(
+        s3Object({
+          bucketName: 'muy-bucket',
+          key: 'path/to/file.txt',
+        }),
+      ),
+    ).resolves.toBeTruthy();
+    expect(spy.mock.calls[0][0]).toMatchInlineSnapshot(`
+      Object {
+        "_helperName": "s3Object",
+        "bucketName": "muy-bucket",
+        "key": "path/to/file.txt",
+      }
+    `);
+
+    spy.mockRestore();
+  });
+
+  it('should not accept invalid inputs', () => {
+    expect(
+      toExistAndMatchObject(
+        appSyncMappingTemplate({
+          template: 'template',
+          context: {},
+        }),
+      ),
+    ).rejects.toThrowErrorMatchingInlineSnapshot(
+      `"appSyncMappingTemplate() is not compatible with the toExistAndMatchObject() matcher"`,
+    );
+  });
+
+  it('should not accept unknown inputs', () => {
+    expect(
+      toExistAndMatchObject({}),
+    ).rejects.toThrowErrorMatchingInlineSnapshot(
+      `"Invalid matcher helper input. Please use one of the provided helpers."`,
+    );
+  });
+});
+
+describe('toExistAndMatchInlineSnapshot', () => {
+  it('should accept dynamodbItem inputs', () => {
+    const spy = jest
+      .spyOn(dynamodb, 'toExistAndMatchInlineSnapshot')
+      .mockResolvedValue({
+        pass: true,
+        message: () => 'message',
+      });
+
+    expect(
+      toExistAndMatchInlineSnapshot(
+        dynamodbItem({
+          tableName: 'table',
+          key: { id: 'id' },
+        }),
+      ),
+    ).resolves.toBeTruthy();
+    expect(spy.mock.calls[0][0]).toMatchInlineSnapshot(`
+      Object {
+        "_helperName": "dynamodbItem",
+        "key": Object {
+          "id": "id",
+        },
+        "tableName": "table",
+      }
+    `);
+
+    spy.mockRestore();
+  });
+
+  it('should accept s3 inputs', () => {
+    const spy = jest
+      .spyOn(s3, 'toExistAndMatchInlineSnapshot')
+      .mockResolvedValue({
+        pass: true,
+        message: () => 'message',
+      });
+
+    expect(
+      toExistAndMatchInlineSnapshot(
+        s3Object({
+          bucketName: 'muy-bucket',
+          key: 'path/to/file.txt',
+        }),
+      ),
+    ).resolves.toBeTruthy();
+    expect(spy.mock.calls[0][0]).toMatchInlineSnapshot(`
+      Object {
+        "_helperName": "s3Object",
+        "bucketName": "muy-bucket",
+        "key": "path/to/file.txt",
+      }
+    `);
+
+    spy.mockRestore();
+  });
+
+  it('should not accept invalid inputs', () => {
+    expect(
+      toExistAndMatchInlineSnapshot(
+        appSyncMappingTemplate({
+          template: 'template',
+          context: {},
+        }),
+      ),
+    ).rejects.toThrowErrorMatchingInlineSnapshot(
+      `"appSyncMappingTemplate() is not compatible with the toExistAndMatchInlineSnapshot() matcher"`,
+    );
+  });
+
+  it('should not accept unknown inputs', () => {
+    expect(
+      toExistAndMatchInlineSnapshot({}),
+    ).rejects.toThrowErrorMatchingInlineSnapshot(
+      `"Invalid matcher helper input. Please use one of the provided helpers."`,
+    );
+  });
+});
+
+describe('toExistAndMatchSnapshot', () => {
+  it('should accept dynamodbItem inputs', () => {
+    const spy = jest
+      .spyOn(dynamodb, 'toExistAndMatchSnapshot')
+      .mockResolvedValue({
+        pass: true,
+        message: () => 'message',
+      });
+
+    expect(
+      toExistAndMatchSnapshot(
+        dynamodbItem({
+          tableName: 'table',
+          key: { id: 'id' },
+        }),
+      ),
+    ).resolves.toBeTruthy();
+    expect(spy.mock.calls[0][0]).toMatchInlineSnapshot(`
+      Object {
+        "_helperName": "dynamodbItem",
+        "key": Object {
+          "id": "id",
+        },
+        "tableName": "table",
+      }
+    `);
+
+    spy.mockRestore();
+  });
+
+  it('should accept s3 inputs', () => {
+    const spy = jest.spyOn(s3, 'toExistAndMatchSnapshot').mockResolvedValue({
+      pass: true,
+      message: () => 'message',
+    });
+
+    expect(
+      toExistAndMatchSnapshot(
+        s3Object({
+          bucketName: 'muy-bucket',
+          key: 'path/to/file.txt',
+        }),
+      ),
+    ).resolves.toBeTruthy();
+    expect(spy.mock.calls[0][0]).toMatchInlineSnapshot(`
+      Object {
+        "_helperName": "s3Object",
+        "bucketName": "muy-bucket",
+        "key": "path/to/file.txt",
+      }
+    `);
+
+    spy.mockRestore();
+  });
+
+  it('should not accept invalid inputs', () => {
+    expect(
+      toExistAndMatchSnapshot(
+        appSyncMappingTemplate({
+          template: 'template',
+          context: {},
+        }),
+      ),
+    ).rejects.toThrowErrorMatchingInlineSnapshot(
+      `"appSyncMappingTemplate() is not compatible with the toExistAndMatchSnapshot() matcher"`,
+    );
+  });
+
+  it('should not accept unknown inputs', () => {
+    expect(
+      toExistAndMatchSnapshot({}),
+    ).rejects.toThrowErrorMatchingInlineSnapshot(
+      `"Invalid matcher helper input. Please use one of the provided helpers."`,
+    );
+  });
+});
